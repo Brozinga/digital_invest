804c56da5f4aa0b391ba6d6a5ac3219f604e3006
brozinga <fnd_broz@hotmail.com>
2021-09-19 05:43:36 -0300

    alteração na documentação

---
+++ assets/database/moedas.js
@@ -0,0 +1,47 @@
+/* 1 */
+{
+    "_id" : ObjectId("6146e17daa6d680a35e87b46"),
+    "nome" : "Dai",
+    "acronimo" : "DAI",
+    "logo" : null
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("6146e186aa6d680a35e87b4b"),
+    "nome" : "Ethereum",
+    "acronimo" : "ETH",
+    "logo" : null
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("6146e18daa6d680a35e87b50"),
+    "nome" : "Chainlink",
+    "acronimo" : "LINK",
+    "logo" : null
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("6146e193aa6d680a35e87b56"),
+    "nome" : "Litecoin",
+    "acronimo" : "LTC",
+    "logo" : null
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("6146e199aa6d680a35e87b5a"),
+    "nome" : "Decentraland",
+    "acronimo" : "MANA",
+    "logo" : null
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("6146e19eaa6d680a35e87b5e"),
+    "nome" : "Polygon",
+    "acronimo" : "MATIC",
+    "logo" : null
+}
