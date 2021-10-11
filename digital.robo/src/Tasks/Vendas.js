e5ce2f5d94ba3008ce11ee10a70875e011103821
brozinga <fnd_broz@hotmail.com>
2021-10-11 02:06:31 -0300

    adicionado o sistema de vendas das moedas

--- digital.robo/src/Tasks/Vendas.js
+++ digital.robo/src/Tasks/Vendas.js
@@ -1,10 +1,57 @@
 const { AsyncTask } = require('toad-scheduler')
 const logger = require("../Utils/logger")(__filename)
+const StatusEnum = require("../../../digital.data/src/Enums/StatusEnum")
 
 module.exports.ID = "JOB_VENDAS"
 
-module.exports.VenderMoedasTask = (MedasRepository,
-    CotacaoRepository,
-    PedidoRepository) => new AsyncTask(
-        
-    );
+module.exports.VenderMoedasTask = (CotacoesRepository,
+    PedidoRepository) => new AsyncTask("Vendas", async () => {
+
+        logger.info("INICIANDO JOB DE VENDAS")
+
+        const pegandoTodosPedidos =
+            await PedidoRepository.find(
+                {
+                    "ativo": true, "status": StatusEnum.ABERTO,
+                    "dataVenda": { $lte: new Date() }
+                }) || [];
+
+        logger.info(`NUMERO DE PEDIDOS DE VENDA ENCONTRADOS: ${pegandoTodosPedidos.length}`)
+
+        const horaAtual = new Date().getHours();
+        let horaVenda = null;
+        let valorVenda = 0;
+
+        for (const item of pegandoTodosPedidos) {
+            horaVenda = item.dataVenda.getHours();
+
+            if (horaVenda <= horaAtual) {
+
+                for (const mC of item.moedasCompra) {
+
+                    const { valorCotado, dataCotacao, moedaId } = await CotacoesRepository.findOne({
+                        moedaId: mC.moedaId
+                    }).sort({ dataCotacao: -1 });
+
+                    item.moedasVenda.push({
+                        valorCotado,
+                        dataCotacao,
+                        moedaId,
+                        quantidade: mC.quantidade
+                    })
+                    valorVenda += Number((valorCotado * mC.quantidade).toFixed(2))
+                }
+
+                item.valorTotalVenda = valorVenda;
+                item.status = StatusEnum.FECHADO;
+
+                logger.info(`PEDIDO Nº: ${item._id}`)
+                logger.info(`VALOR DE COMPRA: ${item.valorTotalCompra}`)
+                logger.info(`VALOR DE VENDA: ${valorVenda}`)
+
+                item.save();
+            }
+
+        }
+
+    });
--- digital.robo/src/index.js
+++ digital.robo/src/index.js
@@ -1,20 +1,28 @@
 const { ToadScheduler, SimpleIntervalJob } = require('toad-scheduler')
 
-const { MoedaRepository, CotacaoRepository } = require('../../digital.data/src/Database')
+const { MoedasRepository, CotacoesRepository, PedidosRepository } = require('../../digital.data/src/Database')
 const { ConvertSecondsToTime } = require("./Utils")
 const logger = require("./Utils/logger")(__filename)
 
 
 const { PegarCotacoesTask, ID: COTACOES_ID } = require('./Tasks/Cotacoes')
+const { VenderMoedasTask, ID: VENDAS_ID } = require('./Tasks/Vendas')
 
 const scheduler = new ToadScheduler()
 
+//JOB DE COTAÇÃO DAS MOEDAS
 const JOB_COTACAO = new SimpleIntervalJob({ seconds: process.env.TEMPO_EXECUCAO_COTACAO },
-    PegarCotacoesTask(MoedaRepository, CotacaoRepository), COTACOES_ID);
+    PegarCotacoesTask(MoedasRepository, CotacoesRepository), COTACOES_ID);
+
+//JOB DE VENDA DAS MOEDAS
+const JOB_VENDA = new SimpleIntervalJob({ seconds: process.env.TEMPO_EXECUCAO_VENDA },
+    VenderMoedasTask(CotacoesRepository, PedidosRepository), VENDAS_ID);
 
 scheduler.addSimpleIntervalJob(JOB_COTACAO)
+scheduler.addSimpleIntervalJob(JOB_VENDA)
 
 scheduler.startById(COTACOES_ID)
 
 logger.info(`---- RODANDO ROBÔS ----`)
 logger.info(`INTERVALO ENTRE AS EXECUÇÕES DE COTAÇÃO: ${ConvertSecondsToTime(process.env.TEMPO_EXECUCAO_COTACAO)}`)
+logger.info(`INTERVALO ENTRE AS EXECUÇÕES DE VENDA: ${ConvertSecondsToTime(process.env.TEMPO_EXECUCAO_VENDA)}`)
