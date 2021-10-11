22e01f59d2e533caacc2585695e874f6170713f7
brozinga <fbrozinga@outlook.com>
2021-10-11 16:33:41 -0300

    ajuste do bug do logger

--- digital.robo/src/Utils/logger.js
+++ digital.robo/src/Utils/logger.js
@@ -1,10 +1,10 @@
 const winston = require('winston')
-const { format } = require('date-fns')
+const dayjs = require('dayjs')
 const path = require("path")
 
 const logger = filename => winston.createLogger({
     level: 'debug',
-    format: winston.format.printf(info => `${format(new Date(), "yyyy-MM-dd HH:mm:ss")} | ${path.basename(filename)} => ${info.message}`),
+    format: winston.format.printf(info => `${dayjs().format("yyyy-MM-dd HH:mm:ss")} | ${path.basename(filename)} => ${info.message}`),
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({
